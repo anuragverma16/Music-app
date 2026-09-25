a0f3206b06de064f646a7b160292811db6098055
anuragverma16 <anuragverma808575@gmail.com>
2026-09-25 12:05:22 +0530

    last commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 //middleware
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
